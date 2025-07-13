7e9b2051a6178fcb10604cd442a83b79603db857
IvelisBecker <ivelisbecker@gmail.com>
2025-07-13 12:40:59 -0500

    Adjusted SideNav login/logout with selected color palette

---
+++ app/ui/fonts.ts
@@ -0,0 +1,9 @@
+import { Inter } from 'next/font/google';
+import { Manrope } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+
+export const manrope = Manrope({ 
+    weight: ['400', '700'],
+    subsets: ['latin'] 
+});
